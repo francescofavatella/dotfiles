8dc4da582ba5f50c0cf5223fa343ca6d79c55d26
Francesco Favatella <34793940+francescofavatella@users.noreply.github.com>
2023-06-07 18:01:12 +0100

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -9,11 +9,17 @@ bindkey \^U backward-kill-line
 # aws
 alias aws-cli="docker run --rm -it amazon/aws-cli"
 
-# pm
 # jdk() { export JAVA_HOME=$(/usr/libexec/java_home -v"$1"); java -version}
-alias java8="jdk 1.8"
 alias java11="jdk 11"
 
 # gradle
 alias gradleDisable="mv  ~/.gradle/init.gradle{,.DISABLED}"
 alias gradleEnable="mv  ~/.gradle/init.gradle{.DISABLED,}"
+
+alias scripts="cd ~/francesco/scripts"
+
+EDITOR="VIM"
+
+# Customize to your needs...
+# source ~/aliases.zsh
+# export PATH=~/bin:$PATH
